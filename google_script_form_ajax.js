fdaf4b76e72487997819e7708e48a47c9a56446b
Chen Liang <chen.albert.liang@gmail.com>
2021-08-05 23:19:27 -0400

    waitlist form test

--- google_script_form_ajax.js
+++ google_script_form_ajax.js
@@ -1,5 +1,5 @@
 var $form = $('form#test-form'),
-    url = 'https://script.google.com/macros/s/AKfycbxo4sKThB130VtqUfXXodH_Yu33NnAIgaj8pwmpjncfTfZEF66yFSwq6EfBWExdYlnF/exec'
+    url = 'https://script.google.com/macros/s/AKfycbyGauk8gM2ye5Z0zf7MqNwcafe7K2KlSomThrHl4O5ZryhCZNkpJW0fLLxIgoL1hok/exec'
 
 $('#submit-form').on('click', function(e) {
   e.preventDefault();
